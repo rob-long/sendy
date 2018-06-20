5d77b3a3e5c82460e525452bb6c535511fda8545
Robert Long <robertlong@Roberts-MacBook-Pro.local>
2018-06-19 18:41:22 -0700

    passport

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+// we only need to run the passport configuration file, there is nothing to use
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send({hi: 'there'});
+
+app.get('/', (req, res) => {
+  res.send({ hi: keys.googleClientSecret });
 });
 
 const PORT = process.env.PORT || 5000;
