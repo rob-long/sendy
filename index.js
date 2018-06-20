f43fa85cc63d21c8af55f338d5211a1b4ffba619
Robert Long <robertlong@Roberts-MacBook-Pro.local>
2018-06-19 19:26:12 -0700

    refactor and add mongoose

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 // we only need to run the passport configuration file, there is nothing to use
 require('./services/passport');
+// const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI);
+console.log(keys.mongoURI);
 
 const app = express();
 
+// more direct way of require authRoutes
+// authRoutes returns a function which we invoke immediately
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
-  res.send({ hi: keys.googleClientSecret });
+  res.send({ hi: 'there' });
 });
 
 const PORT = process.env.PORT || 5000;
---
+++ test.js
@@ -0,0 +1,50 @@
+const MAP = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+  20: 'XX',
+  30: 'XXX',
+  40: 'XL',
+  50: 'L',
+  60: 'LX',
+  70: 'LXX',
+  80: 'LXXX',
+  90: 'XC',
+  100: 'C',
+  200: 'CC',
+  300: 'CCC',
+  400: 'CD',
+  500: 'D',
+  600: 'DC',
+  700: 'DCC',
+  800: 'DCCC',
+  900: 'CM',
+  1000: 'M',
+};
+
+function toRoman(number, places) {
+  if (number < 1 || !places.length) {
+    return '';
+  }
+  const divisor = places.shift();
+  const quotient = Math.floor(number / divisor);
+  const result = quotient ? MAP[divisor].repeat(quotient) : '';
+  return result + toRoman(number % divisor, places);
+}
+
+function solution(number) {
+  const places = Object.keys(MAP).sort((a, b) => a - b).reverse();
+  return toRoman(number, places);
+}
+
+// console.log(solution(2));
+// console.log(solution(3));
+// console.log(solution(1990));
+console.log(solution(2007));
