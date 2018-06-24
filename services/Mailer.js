bfc9e080d5eff366ef967c7c08a2d221366aed2a
Robert Long <robertlong@Roberts-MacBook-Pro.local>
2018-06-23 17:32:09 -0700

    survey routes

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,5 +1,40 @@
-const sendgrid = require("sendgrid");
-const helper = sendgrid.mail;
+const sgMail = require("@sendgrid/mail");
 const keys = require("../config/keys");
+const makeTemplate = require("./emailTemplate");
 
-class Mailer extends helper.Mail {}
+class Mailer {
+  // deconstruct object - can be any object with subject and recipient
+  constructor({ subject, recipients }, content) {
+    sgMail.setApiKey(keys.sendGridKey);
+    this.to = this.formatAddresses(recipients);
+    this.from = "no-reply@sendy.com";
+    this.subject = subject;
+    this.text = content;
+    this.html = makeTemplate(content);
+  }
+
+  getAll() {
+    return Object.keys(this).reduce((acc, cur, i) => {
+      acc[cur] = this[cur];
+      return acc;
+    }, {});
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => email);
+  }
+
+  async send() {
+    const msg = this.getAll();
+    const response = await sgMail.sendMultiple(msg);
+    return response;
+  }
+}
+
+const arrayToObject = array =>
+  array.reduce((obj, item) => {
+    obj[item.id] = item;
+    return obj;
+  }, {});
+
+module.exports = Mailer;
