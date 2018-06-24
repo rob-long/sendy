2e274c57cd8cd68395a71d72fdc7eee44fd88912
Robert Long <robertlong@Roberts-MacBook-Pro.local>
2018-06-23 17:23:25 -0700

    add domain

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
+import surveyReducer from "./surveyReducer";
 
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer,
+  survey: surveyReducer
 });
 
 export default rootReducer;
